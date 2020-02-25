c43b2ffcc6be1bcc938e4cf338370d7e201edcba
OrangeGE <OrangeMGE@users.noreply.github.com>
2020-02-26 00:41:23 +0300

    Rebuild project

--- coursework/README.md
+++ coursework/README.md
@@ -1,13 +1,1 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-#Web application based on
-
-##-HTML
-##-CSS
-##-JS
-##-React
-##-Mobx
-
-#Server-side on
-
-##NodeJS-Express
--- coursework/src/index.js
+++ coursework/src/index.js
@@ -1,19 +1,8 @@
-import React, { Component } from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import DevTools from 'mobx-react-devtools'
 
-class Main extends Component {
-  render() {
-    return(
-      <div>
-        <h1>Максим пидор</h1>
-      </div>
-    );
-  }
-}
 
-ReactDOM.render(<Main />, document.getElementById('root'))
 
 serviceWorker.unregister();
